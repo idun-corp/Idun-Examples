a99a9de599675b3bdbf2c4152c4e52450acbc431
PashaKonietin <pavlokonietin@gmail.com>
2020-09-03 16:22:21 +0300

    simple sample deploy changes

--- ProptechOS-Edge-Interface/examples/java/src/main/java/com/proptechos/SimulatedDeviceApplication.java
+++ ProptechOS-Edge-Interface/examples/java/src/main/java/com/proptechos/SimulatedDeviceApplication.java
@@ -5,15 +5,15 @@ import static java.lang.Thread.currentThread;
 
 import com.microsoft.azure.sdk.iot.device.DeviceClient;
 import com.microsoft.azure.sdk.iot.device.MessageCallback;
+import com.proptechos.model.config.DeviceConfig;
+import com.proptechos.service.JsonParser;
+import com.proptechos.service.actuations.DeviceMessageCallback;
+import com.proptechos.service.observations.MessageSender;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import com.proptechos.model.config.DeviceConfig;
-import com.proptechos.service.JsonParser;
-import com.proptechos.service.actuations.DeviceMessageCallback;
-import com.proptechos.service.observations.MessageSender;
 
 public class SimulatedDeviceApplication {
 
@@ -35,11 +35,6 @@ public class SimulatedDeviceApplication {
     ExecutorService executor = Executors.newSingleThreadExecutor();
     executor.execute(sender);
 
-    // Stop the application.
-    System.out.println("Press ENTER to exit.");
-    System.in.read();
-    executor.shutdownNow();
-    client.closeNow();
   }
 
 }
