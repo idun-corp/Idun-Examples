d1ee7836c8f385dcc9e8ef93dc46c1019b1ca04a
Maria Senkiv <marichka.spin@gmail.com>
2021-02-18 16:00:26 +0200

    Added examples for ProptechOS API version 3.2.2

---
+++ ProptechOS-Java-SDK/version-3.2.2/src/main/java/com/proptechos/clients/AliasNamespaceServiceClient.java
@@ -0,0 +1,34 @@
+package com.proptechos.clients;
+
+import com.proptechos.model.AliasNamespace;
+import com.proptechos.model.common.Paged;
+import com.proptechos.service.AliasNamespaceService;
+import com.proptechos.service.ServiceFactory;
+import com.proptechos.service.filters.common.LitteraFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AliasNamespaceServiceClient {
+
+  private final AliasNamespaceService namespaceService;
+
+  public AliasNamespaceServiceClient(ServiceFactory serviceFactory) {
+    this.namespaceService = serviceFactory.aliasNamespaceService();
+  }
+
+  public Paged<AliasNamespace> listFirstTenAxioms() {
+    return namespaceService.getPage(0, 10);
+  }
+
+  public Paged<AliasNamespace> listAxiomsByLitteraPart(String littera) {
+    return namespaceService
+        .getPageFiltered(0, 10, new LitteraFilter(littera));
+  }
+
+  public AliasNamespace getAxiomById() {
+    AliasNamespace namespace = listFirstTenAxioms().getContent().iterator().next();
+
+    return namespaceService.getById(namespace.getId());
+  }
+
+}
