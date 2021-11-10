249e5b82a881be45772977ef5a108385a83b3f71
Kuzo Nazar <nazar.kuzo@outlook.com>
2021-11-10 16:29:07 +0200

    added console app listener

---
+++ ProptechOS-Streaming-Api/examples/netcore/dedicated-processor/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Logging;
+
+namespace Idun.StreamingApi.Examples.DedicatedProcessor
+{
+    public class Program
+    {
+        private static readonly JsonSerializerOptions JsonSerializerOptions
+            = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, NumberHandling = JsonNumberHandling.AllowReadingFromString };
+
+        private static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var logger = LoggerFactory.Create(logger => logger.AddConsole()).CreateLogger<Program>();
+
+            var storageClient = new BlobContainerClient(
+                configuration.GetValue<string>("BlobStorageConnectionString"),
+                configuration.GetValue<string>("BlobContainerName"));
+
+            var processor = new EventProcessorClient(
+                storageClient,
+                configuration.GetValue<string>("EventHubConsumerGroup"),
+                configuration.GetValue<string>("EventHubConnectionString"),
+                configuration.GetValue<string>("EventHubName"));
+
+            processor.ProcessEventAsync += async eventArgs =>
+            {
+                try
+                {
+                    var sensorObservation = eventArgs.Data.EventBody.ToObjectFromJson<SensorObservation>(JsonSerializerOptions);
+
+                    logger.LogInformation($"Processed a sensor observation:\n" +
+                        $"\tValue:\t\t{sensorObservation.Observation.Value},\n" +
+                        $"\tQuantity Kind:\t{sensorObservation.Observation.QuantityKind},\n" +
+                        $"\tSensor Id:\t{sensorObservation.Observation.SensorId},\n");
+
+                    logger.LogInformation("Press any key to read next one");
+
+                    Console.ReadKey(true);
+
+                    await Task.Yield();
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, $"Failed to processed a message with id: {eventArgs.Data.MessageId}");
+                }
+
+                await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+            };
+
+            processor.ProcessErrorAsync += async eventArgs =>
+            {
+                logger.LogError(eventArgs.Exception, $"Failed to processed a message with on partition: {eventArgs.PartitionId}");
+
+                await Task.Yield();
+            };
+
+            await processor.StartProcessingAsync();
+
+            logger.LogInformation($"You are listening to EventHub stream. Press Escape to stop listener.");
+
+            while (Console.ReadKey(true) is ConsoleKeyInfo key)
+            {
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+            }
+
+            await processor.StopProcessingAsync();
+        }
+    }
+}
---
+++ ProptechOS-Streaming-Api/examples/netcore/dedicated-processor/readme.md
@@ -0,0 +1,23 @@
+# Consuming ProptechOS Streaming API with Dedicated Processor
+
+## Prerequisites
+
+* EventHub Connection String in format
+  `Endpoint=sb://<eventhub-namespace>.servicebus.windows.net/;SharedAccessKeyName=<access-key-name>;SharedAccessKey=<access-key>`
+* EventHub Name
+* Consumer Group name
+* EventHub Name
+* Blob Storage Connection String in format
+  `DefaultEndpointsProtocol=https;AccountName=<storage-account-name>;AccountKey=<storage-account-key>;EndpointSuffix=core.windows.net`
+* Blob Container Name
+
+## How to Run
+
+1. Update `appsettings.json` file and set variables:
+   * EventHubConnectionString
+   * EventHubName
+   * EventHubConsumerGroup (if empty then '$Default' ConsumerGroup will be used)
+   * BlobStorageConnectionString
+   * BlobContainerName
+2. Please ensure you have blob container created
+3. Build & Run
