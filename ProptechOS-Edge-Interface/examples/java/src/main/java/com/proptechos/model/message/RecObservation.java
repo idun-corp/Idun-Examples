28e62d6cbdd47334c50b4844c15544f2309ed57a
Maria Senkiv <marichka.spin@gmail.com>
2022-03-29 07:19:10 +0200

    Plat-2664,2665 Fixed handling number and string value

--- ProptechOS-Edge-Interface/examples/java/src/main/java/com/proptechos/model/message/RecObservation.java
+++ ProptechOS-Edge-Interface/examples/java/src/main/java/com/proptechos/model/message/RecObservation.java
@@ -3,61 +3,77 @@ package com.proptechos.model.message;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.time.ZonedDateTime;
+import java.util.regex.Pattern;
 
 public class RecObservation {
 
-  private final ZonedDateTime observationTime;
-  private final String quantityKind;
-  private final String sensorId;
+    private final ZonedDateTime observationTime;
+    private final String quantityKind;
+    private final String sensorId;
 
-  @JsonInclude(JsonInclude.Include.NON_NULL)
-  private Double value;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Number value;
 
-  @JsonInclude(JsonInclude.Include.NON_NULL)
-  private String valueString;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String valueString;
 
-  @JsonInclude(JsonInclude.Include.NON_NULL)
-  private Boolean valueBoolean;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Boolean valueBoolean;
 
-  public RecObservation(ZonedDateTime observationTime, Object value, String quantityKind,
-      String sensorId) {
-    this.observationTime = observationTime;
-    this.quantityKind = quantityKind;
-    this.sensorId = sensorId;
-    setValue(value);
-  }
-
-  public ZonedDateTime getObservationTime() {
-    return observationTime;
-  }
-
-  public Double getValue() {
-    return value;
-  }
-
-  public String getValueString() {
-    return valueString;
-  }
-
-  public Boolean getValueBoolean() {
-    return valueBoolean;
-  }
-
-  public String getQuantityKind() {
-    return quantityKind;
-  }
-
-  public String getSensorId() {
-    return sensorId;
-  }
-
-  private void setValue(Object value) {
-    if (value instanceof Double) {
-      this.value = (Double) value;
-    } else if (value instanceof String) {
-      this.valueString = (String) value;
-    } else if (value instanceof Boolean) {
-      this.valueBoolean = (Boolean) value;
+    public RecObservation(ZonedDateTime observationTime, Object value, String quantityKind,
+                          String sensorId) {
+        this.observationTime = observationTime;
+        this.quantityKind = quantityKind;
+        this.sensorId = sensorId;
+        setValue(value);
     }
-  }
+
+    public ZonedDateTime getObservationTime() {
+        return observationTime;
+    }
+
+    public Number getValue() {
+        return value;
+    }
+
+    public String getValueString() {
+        return valueString;
+    }
+
+    public Boolean getValueBoolean() {
+        return valueBoolean;
+    }
+
+    public String getQuantityKind() {
+        return quantityKind;
+    }
+
+    public String getSensorId() {
+        return sensorId;
+    }
+
+    private static final Pattern NUMERIC = Pattern.compile("-?\\d+(\\.\\d+)?");
+    private static final Pattern BOOLEAN = Pattern.compile("true|false", Pattern.CASE_INSENSITIVE);
+
+    private void setValue(Object value) {
+        Object resultValue = value instanceof String
+            ? handleString((String) value) : value;
+        if (resultValue instanceof Number) {
+            this.value = (Number) resultValue;
+        } else if (resultValue instanceof Boolean) {
+            this.valueBoolean = (Boolean) resultValue;
+        } else if (resultValue instanceof String) {
+            this.valueString = (String) resultValue;
+        }
+    }
+
+    private Object handleString(String value) {
+        if (NUMERIC.matcher(value).matches()) {
+            return Double.valueOf(value);
+        }
+        if (BOOLEAN.matcher(value).matches()) {
+            return Boolean.parseBoolean(value);
+        }
+        return value;
+    }
 }
